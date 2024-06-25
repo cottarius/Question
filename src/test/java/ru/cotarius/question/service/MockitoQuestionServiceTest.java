7351c0aaa094b12183faeae8b068049d7f419410
cottarius <cotarius@yandex.ru>
2024-06-25 21:44:03 +0300

    Добавлены тесты в MockitoQuestionServiceTest

--- src/test/java/ru/cotarius/question/service/MockitoQuestionServiceTest.java
+++ src/test/java/ru/cotarius/question/service/MockitoQuestionServiceTest.java
@@ -1,13 +1,9 @@
 package ru.cotarius.question.service;
 
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.cotarius.question.entity.Question;
 import ru.cotarius.question.entity.Theme;
@@ -23,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class MockitoServiceTest {
+public class MockitoQuestionServiceTest {
 
     @InjectMocks
     private QuestionService questionService;
@@ -37,15 +33,69 @@ public class MockitoServiceTest {
         Question core1Question = new Question(1L, "Вопрос 1", "Ответ 1", Theme.CORE1, true);
         Question core2Question = new Question(2L, "Вопрос 2", "Ответ 2", Theme.CORE2_COLLECTIONS, false);
         Question core3Question = new Question(3L, "Вопрос 3", "Ответ 3", Theme.CORE3_MULTITHREADING, true);
-        Question dataBaseQuestion = new Question(4L, "Вопрос 4", "Ответ 4", Theme.SQL_DATABASE, false);
-        when(questionRepository.findAll()).thenReturn(Arrays.asList(core1Question, core2Question, core3Question, dataBaseQuestion));
+        Question sqlQuestion = new Question(4L, "Вопрос 4", "Ответ 4", Theme.SQL_DATABASE, false);
+        when(questionRepository.findAll()).thenReturn(Arrays.asList(core1Question, core2Question, core3Question, sqlQuestion));
 
         List<Question> questions = questionService.getPrimaryQuestions();
         assertThat(questions).isNotNull();
         assertThat(questions.stream().allMatch(Question::isImpotent)).isTrue();
         assertThat(questions).hasSize(2);
         assertThat(questions.get(1)).isEqualTo(core3Question);
+    }
 
+    @Test
+    public void getSpringQuestionsTest(){
+        Question springQuestion = new Question(1L, "Вопрос 3", "Ответ 3", Theme.SPRING, true);
+        Question sqlQuestion = new Question(2L, "Вопрос 4", "Ответ 4", Theme.SQL_DATABASE, false);
+        Question hibernateQuestion = new Question(3L, "Вопрос 5", "Ответ 5", Theme.HIBERNATE_JDBC, true);
+        when(questionRepository.findAll()).thenReturn(Arrays.asList(springQuestion, sqlQuestion, hibernateQuestion));
+
+        List<Question> questions = questionService.getSpringQuestions();
+        assertThat(questions).isNotNull();
+        assertThat(questions.stream().allMatch(q -> q.getTheme().equals(Theme.SPRING))).isTrue();
+        assertThat(questions).hasSize(1);
+    }
+
+    @Test
+    public void getHibernateQuestionsTest(){
+        Question core3Question = new Question(3L, "Вопрос 3", "Ответ 3", Theme.CORE3_MULTITHREADING, true);
+        Question sqlQuestion = new Question(4L, "Вопрос 4", "Ответ 4", Theme.SQL_DATABASE, false);
+        Question hibernateQuestion = new Question(5L, "Вопрос 5", "Ответ 5", Theme.HIBERNATE_JDBC, true);
+        when(questionRepository.findAll()).thenReturn(Arrays.asList(core3Question, sqlQuestion, hibernateQuestion));
+
+        List<Question> questions = questionService.getHibernateQuestions();
+        assertThat(questions).isNotNull();
+        assertThat(questions.stream().allMatch(q -> q.getTheme().equals(Theme.HIBERNATE_JDBC))).isTrue();
+        assertThat(questions).hasSize(1);
+    }
+
+    @Test
+    public void getSqlQuestionsTest(){
+        Question core3Question = new Question(3L, "Вопрос 3", "Ответ 3", Theme.CORE3_MULTITHREADING, true);
+        Question sqlQuestion = new Question(4L, "Вопрос 4", "Ответ 4", Theme.SQL_DATABASE, false);
+        Question hibernateQuestion = new Question(5L, "Вопрос 5", "Ответ 5", Theme.HIBERNATE_JDBC, true);
+        when(questionRepository.findAll()).thenReturn(Arrays.asList(core3Question, sqlQuestion, hibernateQuestion));
+
+        List<Question> questions = questionService.getSqlQuestions();
+        assertThat(questions).isNotNull();
+        assertThat(questions.stream().allMatch(q -> q.getTheme().equals(Theme.SQL_DATABASE))).isTrue();
+        assertThat(questions).hasSize(1);
+    }
+
+    @Test
+    public void getCore3QuestionsTest(){
+        Question core1Question = new Question(1L, "Вопрос 1", "Ответ 1", Theme.CORE1, true);
+        Question core2Question = new Question(2L, "Вопрос 2", "Ответ 2", Theme.CORE2_COLLECTIONS, false);
+        Question core3Question = new Question(3L, "Вопрос 3", "Ответ 3", Theme.CORE3_MULTITHREADING, true);
+        when(questionRepository.findAll()).thenReturn(Arrays.asList(core1Question, core2Question, core3Question));
+
+        List<Question> questions = questionService.getCore3Questions();
+
+        assertThat(questions.stream().allMatch(q -> q.getTheme().equals(Theme.CORE3_MULTITHREADING))).isTrue();
+
+        assertThat(questions).hasSize(1);
+        assertThat(questions.get(0)).isEqualTo(core3Question);
+        assertThat(questions).isNotNull();
     }
 
     @Test
@@ -96,15 +146,5 @@ public class MockitoServiceTest {
         List<Question> questions = questionService.findAll();
         assertEquals(2, questions.size());
         assertThat(questions).isNotNull();
-    }
-
-    private List<Question> getQuestions(Theme theme) {
-        List<Question> hibernateQuestions = new ArrayList<>();
-        for (Question question : questionService.findAll()) {
-            if (question.getTheme() == theme) {
-                hibernateQuestions.add(question);
-            }
-        }
-        return hibernateQuestions;
     }
 }
