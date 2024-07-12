4428166712f0f6aebd25c7a9c137bbec360e3386
cottarius <cotarius@yandex.ru>
2024-07-12 17:15:39 +0300

    ..

--- src/main/java/ru/cotarius/question/QuestionApplication.java
+++ src/main/java/ru/cotarius/question/QuestionApplication.java
@@ -1,6 +1,5 @@
 package ru.cotarius.question;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuestionApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+//        Dotenv dotenv = Dotenv.configure().load();
+//        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
         SpringApplication.run(QuestionApplication.class, args);
     }
