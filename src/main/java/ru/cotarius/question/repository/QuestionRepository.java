0765f90d990eb01257adb57348cef0c4aea43438
Oleg Prokopenko <cotarius@yandex.ru>
2025-04-19 21:10:12 +0300

    javadoc для QuestionRepository, UserRepository

--- src/main/java/ru/cotarius/question/repository/QuestionRepository.java
+++ src/main/java/ru/cotarius/question/repository/QuestionRepository.java
@@ -3,5 +3,10 @@ package ru.cotarius.question.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.cotarius.question.entity.Question;
 
+/**
+ * Репозиторий для работы с сущностью Question.
+ *
+ * @author olegprokopenko
+ */
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
--- src/main/java/ru/cotarius/question/repository/UserRepository.java
+++ src/main/java/ru/cotarius/question/repository/UserRepository.java
@@ -5,6 +5,11 @@ import ru.cotarius.question.entity.User;
 
 import java.util.Optional;
 
+/**
+ * Репозиторий для работы с сущностью User.
+ *
+ * @author olegprokopenko
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
